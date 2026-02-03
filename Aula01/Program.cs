28d6fd1e680622cbdb9bb8f4e5e09a66db88d5c6
23011129 <23011129@305-18>
2026-02-02 22:34:50 -0300

    Adicionar arquivos de projeto.

---
+++ Aula01/Program.cs
@@ -0,0 +1,35 @@
+﻿Double NI, PI, PO, Media;
+
+Console.WriteLine("NI: ");
+NI = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("PI: ");
+PI = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("PO: ");
+PO = Convert.ToDouble(Console.ReadLine());
+
+Media = (NI*0.20) + (PI*0.30) + (PO*0.50);
+
+if (Media >= 6) 
+{
+    Console.Write("Aprovado: ");
+}
+
+if (Media >= 4.0 && Media < 6.0)
+{
+    Console.Write("Exame - nota 6: ");
+}
+
+if (Media >= 2 && Media < 4)
+{
+    Console.Write("Exame - nota 7: ");
+}
+
+if (Media < 2)
+{
+    Console.Write("Reprovado: ");
+}
+
+
+Console.WriteLine("A média final é " + Media);
