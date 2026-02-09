d34775b22d22bc05ceb486af4b71473bdc8b2940
Laura Pelizzer <161631961+LauraPiv@users.noreply.github.com>
2026-02-09 20:12:02 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+Int64 fatorial(Int64 n)
+{
+    int fat = 1;
+    for (Int32 i = 1; i <= n; i++)
+    {
+        fat = fat * i;
+    }
+    return fat;
+}
+
+Int16 N;
+Console.WriteLine("Fatorial de N: ");
+N = Int16.Parse(Console.ReadLine());
+Int64 resutado = fatorial(N);
+Console.WriteLine(N + "! = " + resutado);
+
